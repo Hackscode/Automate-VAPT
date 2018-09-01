222b5b17e904c77b29d562735b5a0eaa396540bd
Hackscode <hackscode@protonmail.ch>
2018-09-01 18:45:33 +0530

    Update microsoft.sh

--- scripts/microsoft.sh
+++ scripts/microsoft.sh
@@ -2,5 +2,5 @@ mkdir -p `pwd`/microsoft-ds/enum4linux/
 mkdir -p `pwd`/microsoft-ds/smb_security_mode/
 echo $1
 IFS=':' read var3 var4 <<< "$1"
-enum4linux -a $1 > `pwd`/microsoft-ds/enum4linux/$1
+enum4linux -a $var3 > `pwd`/microsoft-ds/enum4linux/$1
 nmap -Pn -n --script=smb-security-mode.nse $var3 -p $var4 -oN `pwd`/microsoft-ds/smb_security_mode/$1
