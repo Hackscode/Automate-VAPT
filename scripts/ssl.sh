2ec5619e3db2df8ed78c466feef29a88c90c5d89
Hackscode <hackscode@protonmail.ch>
2018-09-08 12:42:17 +0530

    Update ssl.sh

--- scripts/ssl.sh
+++ scripts/ssl.sh
@@ -1,6 +1,10 @@
 mkdir -p `pwd`/ssl/ssl_enum/
 mkdir -p `pwd`/ssl/testssl/
+mkdir -p `pwd`/ssl/ssl_cert/
+mkdir -p `pwd`/ssl/cert_intaddr/
 echo $1
 IFS=':' read var3 var4 <<< "$1"
 bash ../scripts/testssl/testssl.sh $1 > `pwd`/ssl/testssl/$1
 nmap -Pn -n --script=ssl-enum-ciphers $var3 -p $var4 -oN `pwd`/ssl/ssl_enum/$1
+nmap -Pn -n --script=ssl-cert $var3 -p $var4 -oN `pwd`/ssl/ssl_cert/$1
+nmap -Pn -n --script=ssl-cert-intaddr $var3 -p $var4 -oN `pwd`/ssl/ssl_intaddr/$1
