76304ef41e055971c0e002ba008cacb5f0c5f9f6
Hackscode <hackscode@protonmail.ch>
2019-02-18 13:22:10 +0530

    Update http.sh

--- scripts/http.sh
+++ scripts/http.sh
@@ -12,4 +12,4 @@ nmap -Pn -n --script=http-trace $var1 -p $var2 -d -oN `pwd`/http/http_trace/$1
 nmap -Pn -n --script=http-methods $var1 -p $var2 -d -oN `pwd`/http/http_methods/$1
 nmap -Pn -n --script=http-headers $var1 -p $var2 -d -oN `pwd`/http/http_headers/$1
 /usr/bin/perl /usr/bin/nikto -h $var1 -p $var2 -F txt -o `pwd`/http/nikto/$1
-python ../EyeWitness/EyeWitness.py --headless --no-prompt --single "$1" -d `pwd`/http/EyeWitness/$1
+eyewitness --web --no-prompt --single "$1" -d `pwd`/http/EyeWitness/$1
